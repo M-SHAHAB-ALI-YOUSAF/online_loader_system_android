8cc441d77aa9278b3e60e269ce578b69ac2b4fc7
M SHAHAB ALI YOUSAF <mshahabaliyousaf414@gmail.com>
2024-02-01 12:54:01 +0500

    update driver status

--- app/src/main/java/com/shahab12344/loader_system/Driver_Homepage_Fragment.java
+++ app/src/main/java/com/shahab12344/loader_system/Driver_Homepage_Fragment.java
@@ -13,7 +13,20 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver_Homepage_Fragment extends Fragment {
     private ToggleButton toggleButtonStatus;
@@ -84,14 +97,65 @@ public class Driver_Homepage_Fragment extends Fragment {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // Handle status change here
                 if (isChecked) {
+                    UpdateStatusToActive("True");
                     Toast.makeText(getContext(), "ONLINE", Toast.LENGTH_SHORT).show();
                     linearLayoutOffline.setVisibility(View.GONE);
 
                 } else {
+                    UpdateStatusToActive("False");
                     linearLayoutOffline.setVisibility(View.VISIBLE);
                 }
             }
         });
         return view;
     }
+
+    private void UpdateStatusToActive(String Status) {
+            String  url = "http://10.0.2.2/Cargo_Go/v1/updateActiveStatusOfDriver.php";
+            // Create a HashMap to hold the updated user data
+            Map<String, String> params = new HashMap<>();
+            params.put("Driver_Phone_No", sessionManager.getPhoneNumber());
+            params.put("is_Active", Status);
+
+            // Send a POST request to your PHP server for updating the user profile
+            RequestQueue queue = Volley.newRequestQueue(requireContext());
+        //        String url = "http://10.0.2.2/Cargo_Go/v1/updateUser.php"; // Replace with your server URL
+            StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                    url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean error = jsonResponse.getBoolean("error");
+                                String message = jsonResponse.getString("message");
+
+                                if (!error) {
+                                    if(Status == "True"){
+                                        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
+                                    }
+
+                                } else {
+                                    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(requireContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(requireContext(), "Network Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    return params;
+                }
+            };
+
+            queue.add(stringRequest);
+        }
 }
